5360f2c2a6dcf33c512390e2af2779cbfe469df4
almene <52462641+almene@users.noreply.github.com>
2020-02-12 13:28:32 -0500

    generating testing scripts

---
+++ tests/test_parse_tree.py
@@ -0,0 +1,12 @@
+from bioCanon import bioCanon
+import os
+from ete3 import Tree
+
+
+def test_parse_tree():
+    tree_file = os.path.join(os.getcwd(),"tests", "examples", "testing.nwk")
+    compare = Tree("((((((((((((Q,R),(O,P)),J),I),H),G),(S,F)),B),(C,(T,U))),"
+                   "(((M,N),(K,L)),E)),(D,V)),A);")
+    case = bioCanon.parse_tree(tree_file)
+    assert print(case) == print(compare)
+
