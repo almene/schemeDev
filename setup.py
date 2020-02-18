f831bebb4f69935f47657e5edf98380946d4dac9
almene <52462641+almene@users.noreply.github.com>
2020-02-18 13:24:00 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bioCanon-almene", # Replace with your own username
+    version="0.0.1",
+    author="Amanda Saunders",
+    author_email="saunders.mandy@hotmail.com",
+    description="A package for generating potential biohansel schemes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/almene/schemeDev",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
